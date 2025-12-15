3872b0966c2712042fe3868d0697f3f8446a7f78
Tokami <nacer354@hotmail.com>
2025-12-15 11:02:25 -0400

    admin ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react'; // <-- Importez useEffect
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import netlifyIdentity from 'netlify-identity-widget'; // <-- Importez netlifyIdentity
 import { LanguageProvider } from './context/LanguageContext';
 import { DataProvider } from './context/DataContext';
 import Header from './components/Header';
@@ -14,31 +12,6 @@ import BlogPostPage from './pages/BlogPostPage';
 import MuseumPage from './pages/MuseumPage';
 
 function App() {
-  
-  // --- NOUVEAU CODE D'INITIALISATION ---
-  useEffect(() => {
-    // 1. Initialiser Netlify Identity
-    // Cette fonction scanne automatiquement l'URL (le hash #invite_token=...)
-    // et ouvre la modale de création de mot de passe si un token est présent.
-    netlifyIdentity.init(); 
-
-    // Optionnel mais recommandé : Écouter la connexion pour rediriger
-    netlifyIdentity.on('login', () => {
-        // Rediriger vers l'interface d'administration après connexion
-        // Ceci est crucial si vous utilisez Netlify CMS
-        netlifyIdentity.close();
-        // Optionnel: window.location.href = '/admin/';
-    });
-    
-    // Nettoyage : retirer l'écouteur si le composant est démonté
-    return () => {
-        netlifyIdentity.off('login');
-    };
-  }, []); // [] garantit que cela s'exécute uniquement au montage
-
-  // --- FIN DU NOUVEAU CODE ---
-
-
   return (
     <HelmetProvider>
       <LanguageProvider>
